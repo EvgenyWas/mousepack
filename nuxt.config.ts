4aad55c70c676636b72ec32ede4952e97e1a8e49
EvgenyWas <evgeniywas@gmail.com>
2024-03-08 18:27:19 +0000

    feat: set up nuxtjs/sitemap module and nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    pageTransition: { name: 'fade', mode: 'out-in' },
+  },
+  site: {
+    url: 'https://mousepack.com',
+    name: 'MousePack',
+    defaultLocale: 'en',
+  },
+  sitemap: {
+    xsl: false,
+    runtimeCacheStorage: false,
+    autoLastmod: true,
+    cacheMaxAgeSeconds: 60 * 60 * 24,
+  },
+  routeRules: { '/**': { static: true } },
+  experimental: {
+    payloadExtraction: false,
+    renderJsonPayloads: false,
+  },
+  typescript: {
+    typeCheck: true,
+  },
+  modules: ['@nuxtjs/sitemap'],
+  ssr: true,
+});
