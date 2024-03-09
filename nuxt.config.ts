f9db0bbc6cd6ed7367e6c6d0fd3ff92a500e0a4d
EvgenyWas <evgeniywas@gmail.com>
2024-03-09 10:57:47 +0000

    feat: connected font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@
 export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'fade', mode: 'out-in' },
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', crossorigin: '', href: 'https://fonts.gstatic.com' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap',
+        },
+      ],
+    },
   },
   site: {
     url: 'https://mousepack.com',
