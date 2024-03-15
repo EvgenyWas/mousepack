6a80e5c96700a4ca87c51e94087e1d72a8ee9487
EvgenyWas <evgeniywas@gmail.com>
2024-03-15 23:17:55 +0000

    feat: created icon components

---
+++ components/icons/IconHeaderShape.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg class="mousepack-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 160" aria-hidden="true">
+    <path d="M0,0V51H72c48,0,88,82.76,178,82.76S380,51,428,51h72V0Z" />
+  </svg>
+</template>
