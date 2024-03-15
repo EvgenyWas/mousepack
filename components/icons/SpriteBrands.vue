dcd6389e683000d28d46ba9d8fc8d9b93dcad208
EvgenyWas <evgeniywas@gmail.com>
2024-03-15 23:18:24 +0000

    feat: created sprite brands component

---
+++ components/icons/SpriteBrands.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg class="mousepack-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img">
+    <use :href="href" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import sprite from '~/assets/icons/sprite-brands.svg';
+
+const props = defineProps<{ name: string }>();
+
+const href = computed<string>(() => sprite + '#' + props.name);
+</script>
