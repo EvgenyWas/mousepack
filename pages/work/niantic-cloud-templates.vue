81ccc09562d2f304c57b421165cc20a7ffb85e2f
EvgenyWas <evgeniywas@gmail.com>
2024-06-14 19:11:37 +0100

    feat: add default work pages

---
+++ pages/work/niantic-cloud-templates.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <h1 class="row text-h2">Cloud Templates</h1>
+    <p class="row text-subtitle">Niantic</p>
+
+    <section class="row">
+      <p class="text-subtitle">
+        To help spark new users ....MousePack created 3 unique game experiences that can be used to make an infinite
+        number of experiences.
+      </p>
+    </section>
+
+    <section class="row">
+      <p class="text-subtitle">
+        Our three game edits include “Botanical Bunny,” “Bog Blasters,” and “Shooter.” Each game features it’s own
+        unique setting, components and game play.
+      </p>
+    </section>
+
+    <section class="row">
+      <p class="text-subtitle">
+        Developers will have fun adjusting the components to create their own versions of these games. Try it on Cloud
+        today!
+      </p>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: 'MousePack | Niantic Cloud Templates',
+  description:
+    // eslint-disable-next-line max-len
+    'To help spark new users ....MousePack created 3 unique game experiences that can be used to make an infinite number of experiences. Our three game edits include “Botanical Bunny,” “Bog Blasters,” and “Shooter.” Each game features it’s own unique setting, components and game play. Developers will have fun adjusting the components to create their own versions of these games. Try it on Cloud today!',
+});
+</script>
+
+<style scoped lang="scss">
+.container {
+  min-height: 100vh;
+  padding-top: 80px;
+}
+</style>
---
+++ pages/work/snapchat-acl.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1 class="row text-h2">Austin City Limits</h1>
+    <p class="row text-subtitle">Snapchat</p>
+
+    <section class="row">
+      <!-- Image -->
+    </section>
+
+    <section class="row">
+      <p class="text-subtitle">Rock out with our funky new Lenses created for the festival, Austicn City limits.</p>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: 'MousePack | Snapchat Austin City Limits',
+  description: 'Rock out with our funky new Lenses created for the festival, Austicn City limits.',
+});
+</script>
+
+<style scoped lang="scss">
+.container {
+  min-height: 100vh;
+  padding-top: 80px;
+}
+</style>
