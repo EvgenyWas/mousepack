19b3b718ff3515eb792bb4a32cc97a4fdbb415b8
EvgenyWas <evgeniywas@gmail.com>
2024-06-30 23:43:03 +0100

    refactor: update connecting google font not to block LCP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,8 +17,11 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', crossorigin: '', href: 'https://fonts.gstatic.com' },
         {
-          rel: 'stylesheet',
+          rel: 'preload',
           href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap',
+          as: 'style',
+          type: 'text/css',
+          onload: 'this.onload=null;this.rel="stylesheet"',
         },
       ],
     },
